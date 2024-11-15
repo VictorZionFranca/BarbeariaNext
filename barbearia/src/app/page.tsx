f398cf08383b5ee6b359f582b692c3190c83bfa6
Victor Zion França <vzion435@gmail.com>
2024-11-15 14:11:33 -0300

    Inicializando Next

---
+++ barbearia/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center text-primary">Hello, Bootstrap CDN!</h1>
+      <button className="btn btn-primary">Clique aqui</button>
+    </div>
+  );
+}
